4c4cec4012630dfbfd24dbbbfdeb76eb09415213
Dmytro <ADimaso007@gmail.com>
2024-06-02 19:18:06 +0300

    Добавленно заполнение DataGridView

--- Entry.cs
+++ Entry.cs
@@ -9,5 +9,10 @@ namespace FinanceManager
         public float  Money { get; set; }
         public string Category { get; set; }
         public string Note { get; set; }
+
+        public string getDate()
+        {
+            return $"{Date.Date} {Date.Hour}:{Date.Minute}";
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.IO;
+using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace FinanceManager
@@ -19,10 +21,9 @@ namespace FinanceManager
             
             LoadData();
             
-            CreditsDGV.AutoResizeColumns();
-            CreditsDGV.AutoResizeRows();
-            DebtsDGV.AutoResizeColumns();
-            DebtsDGV.AutoResizeRows();
+            
+
+
         }
 
 
@@ -53,7 +54,36 @@ namespace FinanceManager
 
         void FillDVGs()
         {
+            var Debts   = (from entrie in entries where entrie.Money  > 0 select entrie).ToList(); 
+            var Credits = (from entrie in entries where entrie.Money <= 0 select entrie).ToList();
 
+            DebtsDGV.RowCount = Debts.Count;
+            CreditsDGV.RowCount = Credits.Count;
+
+            for (int i = 0; i < Debts.Count; i++)
+            {
+                DebtsDGV.Rows[i].Cells[0].Value = Debts[i].getDate();
+                DebtsDGV.Rows[i].Cells[1].Value = Debts[i].Money;
+                DebtsDGV.Rows[i].Cells[2].Value = Debts[i].Category;
+                DebtsDGV.Rows[i].Cells[3].Value = Debts[i].Note;
+            }
+            for (int i = 0; i < Credits.Count; i++)
+            {
+                CreditsDGV.Rows[i].Cells[0].Value = Credits[i].getDate();
+                CreditsDGV.Rows[i].Cells[1].Value = Credits[i].Money;
+                CreditsDGV.Rows[i].Cells[2].Value = Credits[i].Category;
+                CreditsDGV.Rows[i].Cells[3].Value = Credits[i].Note;
+            }
+
+            new Thread(() => { 
+                Thread.Sleep(50);
+                Invoke((MethodInvoker)(() => {
+                    CreditsDGV.AutoResizeColumns();
+                    CreditsDGV.AutoResizeRows();
+                    DebtsDGV.AutoResizeColumns();
+                    DebtsDGV.AutoResizeRows();
+                }));
+            }).Start();
         }
 
         #endregion
